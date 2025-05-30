8d4c0e563a29f4d55d27f279039396dc9edbb277
canibe2 <rivayne91@gmail.com>
2025-05-30 16:53:11 +0900

    회원가입 페이지 구조 작성

--- back/src/main/java/com/housing/back/controller/AuthController.java
+++ back/src/main/java/com/housing/back/controller/AuthController.java
@@ -19,8 +19,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
