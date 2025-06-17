3d0faa7b212b6bb2b3be28f08c21d0404d034f07
canibe2 <rivayne91@gmail.com>
2025-06-17 09:46:01 +0900

    로그인 페이지 작성 / 로그인 API 연동

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import SignIn from 'views/Authentication/SignIn';
 import SignUp from 'views/Authentication/SignUp';
+import { CookiesProvider } from 'react-cookie';
 
 function App() {
 
 
   return (
+    <CookiesProvider>
     <Routes>
       <Route path='/auth'>
         <Route path='sign-up' element={<SignUp/>}/>
+        <Route path='sign-in' element={<SignIn/>}/>
       </Route>
     </Routes>
+    </CookiesProvider>
   );
 };
 
--- front/src/apis/request/auth/index.ts
+++ front/src/apis/request/auth/index.ts
@@ -2,6 +2,7 @@ import SignUpRequestDto from './sign-up.request.dto';
 import IdCheckRequestDto from './id-check.request.dto';
 import EmailCertificationRequestDto from './email-certification.request.dto';
 import CheckCertificationRequestDto from './check-certification.request.dto';
+import SignInRequestDto from './sign-in.request.dto';
 
 export type {
 
@@ -9,4 +10,5 @@ export type {
     IdCheckRequestDto,
     EmailCertificationRequestDto,
     CheckCertificationRequestDto,
+    SignInRequestDto
 }
